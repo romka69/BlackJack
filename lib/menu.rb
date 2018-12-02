c7e8d19940b01c195c04d72319e1eb6e9f70c9cd
roman <romka69tver@gmail.com>
2018-12-02 20:37:44 +0300

    add class menu

---
+++ lib/menu.rb
@@ -0,0 +1,54 @@
+require_relative 'deck'
+require_relative 'player'
+require_relative 'dealer'
+
+class Menu
+
+  def initialize
+    puts "Добро пожаловать в игру Black Jack ver.TN!"
+    @deck = Deck.new
+    @player = Player.new(get_name)
+    @dealer = Dealer.new("Dealer")
+    new_game
+  end
+  
+  def get_name
+    print "Введите ваше имя: "
+    name = gets.chomp.to_s
+  end
+
+  def new_game
+    @deck.shuffle_deck
+    2.times { take_card(@player) }
+    2.times { take_card(@dealer) }
+    see_hand(@player)
+    see_hand(@dealer)
+    see_score
+    @player.place_bet
+    @dealer.place_bet
+    user_move
+  end
+
+  def take_card(name)
+    card = @deck.give_card
+    name.take_card(card)
+  end
+
+  def see_hand(obj)
+    if obj.name == "Dealer"
+      puts "Карт в руке диллера: #{'*' * @dealer.hand.size}."
+    else
+      cards = []
+      @player.hand.each { |card| cards << [card.val + card.suit] }
+      puts "#{@player.name}, у вас в руке: #{cards.join(", ")}."
+    end
+  end
+
+  def see_score
+    puts "Incoming....."
+  end
+
+  def user_move
+    puts "Incoming....."
+  end
+end
