d5b23f17f9230186f9878cf3e76233a9ae4ad45d
roman <romka69tver@gmail.com>
2018-12-02 20:36:31 +0300

    add class deck

---
+++ lib/deck.rb
@@ -0,0 +1,33 @@
+require_relative 'card'
+
+class Deck
+
+  CARD_VALS = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  SUITS = %w(♤ ♡ ♧ ♢)
+
+  attr_reader :deck
+
+  def initialize
+    @deck = create_deck
+  end
+
+  def create_deck
+    arr_handler = []
+
+    CARD_VALS.each do |val|
+      SUITS.each do |suit|
+        arr_handler << Card.new(val, suit)
+      end
+    end
+
+    arr_handler
+  end
+
+  def shuffle_deck
+    @deck.shuffle!
+  end
+
+  def give_card
+    @deck.pop
+  end
+end
