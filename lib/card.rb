f07fec05cd6f2fbc82b544b9ec903788805918ae
roman <romka69tver@gmail.com>
2018-12-08 01:15:03 +0300

    del '.class == Integer'

--- lib/card.rb
+++ lib/card.rb
@@ -9,7 +9,7 @@ class Card
   def score_card
     return 10 if %w[J Q K].include?(@val)
     return 11 if 'A'.include?(@val)
-    return @val.to_i if @val.to_i.class == Integer
+    return @val.to_i if @val.to_i
   end
 
   def ace?
